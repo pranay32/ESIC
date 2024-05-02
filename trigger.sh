3ba4268e6f0804a6c122362cae3cce30e8e986df
Pranay <86782301+findpranay@users.noreply.github.com>
2024-05-02 11:38:16 +0530

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -7,5 +7,5 @@ bearer="Bearer ${token}"
 projectName='project1'
 suiteName='onlyStep'
 response1=$(curl -X POST -H "Authorization: ${bearer}" -H "Content-Type: multipart/form-data" -F "suitename=${suiteName}" -F "projectname=${projectName}" -F "file=@/home/ec2-user/ata_server/suite.csv" https://devserver.icodetest.com:8080/execute/pipeline/suite)
-
+echo $response1
 
